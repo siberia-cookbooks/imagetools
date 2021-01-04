1b391e0ca4a04eb5a2f3f9e09b737506b7c83c3a
Jacques Marneweck <jacques@siberia.co.za>
2021-01-04 08:47:58 +0200

    Update email address

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,6 @@
 name             'imagetools'
 maintainer       'Jacques Marneweck'
-maintainer_email 'jacques@powertrip.co.za'
+maintainer_email 'jacques@siberia.co.za'
 license          'All rights reserved'
 description      'Installs/Configures imagetools'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
